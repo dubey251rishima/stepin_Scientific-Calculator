c5d3dfdb12ab939d4aed78ff05ab4b8d2bfe112f
dubey251rishima <73350873+dubey251rishima@users.noreply.github.com>
2021-09-08 05:04:55 -0700

    Update ADD.c

--- Implementation/src/ADD.c
+++ Implementation/src/ADD.c
@@ -1,1 +1,7 @@
-
+#include "calculator.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+error_t add(arthimetic_inputs* ai){
+    ai->output=(ai->input_1)+(ai->input_2);
+    return SUCCESS;
